5deef637c56964e1230b92f157a234bbac995543
Anzola Fabio <anzola.fabio@gmail.com>
2021-02-14 16:41:14 +0100

    Database diagramm & schema

--- database/schema.sql
+++ database/schema.sql
@@ -6,7 +6,9 @@ USE quiz;
 
 CREATE TABLE Subject (
 	pk_subject_id int AUTO_INCREMENT PRIMARY KEY,
-	subject varchar(255)
+	subject varchar(255),
+	fk_pk_subject_id int,
+	FOREIGN KEY (fk_pk_subject_id) REFERENCES Subject(pk_subject_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Question (
@@ -24,21 +26,31 @@ CREATE TABLE Answer (
 	FOREIGN KEY (fk_pk_question_id) REFERENCES Question(pk_question_id) ON DELETE CASCADE
 );
 
-INSERT INTO `Subject`(`pk_subject_id`, `subject`) VALUES
-(1, 'Math'),
-(2, 'German'),
-(3, 'English'),
-(4, 'Physics'),
-(5, 'Biology'),
-(6, 'Science'),
-(7, 'Physical Education'),
-(8, 'Music'),
-(9, 'Journalism'),
-(10, 'Economics');
+CREATE TABLE User (
+	pk_user_id int AUTO_INCREMENT PRIMARY KEY,
+	firstname varchar(255),
+	lastname varchar(255),
+	username varchar(255),
+	passphrase varchar(255) 
+);
+
+
+INSERT INTO `Subject`(`pk_subject_id`, `subject`, `fk_pk_subject_id`) VALUES
+(1, 'Math', NULL),
+(2, 'German', NULL),
+(3, 'English', NULL),
+(4, 'Physics', NULL),
+(5, 'Biology', NULL),
+(6, 'Science', NULL),
+(7, 'Physical Education', NULL),
+(8, 'Music', NULL),
+(9, 'Journalism', NULL),
+(10, 'Economics', NULL),
+(11, 'Statistic', 1);
 
 INSERT INTO `Question`(`pk_question_id`, `question`, `fk_pk_subject_id`) VALUES
 (1, 'How much is 1 + 1?', 1),
-(2, 'How much is (1 / 3) * 2?', 1),
+(2, 'How much is (1 / 3) * 2?', 11),
 (3, 'How much is 1 / 0?', 1),
 (4, 'How much is 2 ^ 6?', 1);
 
@@ -60,6 +72,5 @@ INSERT INTO `Answer`(`pk_answer_id`, `answer`, `correct`, `fk_pk_question_id`) V
 (15, '100', false, 4),
 (16, '256', false, 4);
 
-
-
-
+INSERT INTO `User`(`pk_user_id`, `firstname`, `lastname`, `username`, `passphrase`) VALUES
+(1, 'Fabio', 'Anzola', 'fabioanzola', MD5('junioradmin'));
